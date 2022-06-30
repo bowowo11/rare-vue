d48e5c91418a635b72c213aa78a71c85daaa004e
huzihao <hzh553871310@163.com>
2022-06-30 18:30:41 +0800

    beibao

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/mainInterface">主界面</router-link> |
-      <router-link to="/chouKa">抽卡</router-link>
+      <router-link to="/chouKa">抽卡</router-link> |
+      <router-link to="/backPack">背包</router-link>
     </nav>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
