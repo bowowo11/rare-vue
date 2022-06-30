b591687178a607970aff94a79d47f0f3bbf5668d
zxear <2602388192@qq.com>
2022-06-30 20:42:05 +0800

    11

--- src/views/chouKa.vue
+++ src/views/chouKa.vue
@@ -3,14 +3,31 @@
     <el-container>
       <el-header>
         <el-container>
-          <div></div><div></div>
+          <div id="msg">个人信息：{{ msg }}</div>
+          <el-container>
+            <el-button type="primary" round id="diamond">钻石：{{ diamond }}</el-button>
+            <el-button type="primary" round id="recharge">充值</el-button>
+          </el-container>
         </el-container>
       </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+
+        </el-aside>
         <el-container>
-          <el-main>Main</el-main>
-          <el-footer>Footer</el-footer>
+          <el-main>
+            <el-carousel height="410px">
+              <el-carousel-item v-for="item in imgWrap" :key="item.url">
+                <img :src="item.url" class="picture"/>
+              </el-carousel-item>
+            </el-carousel>
+          </el-main>
+          <el-footer>
+            <el-container>
+              <el-button type="primary" round id="lottery01">来发单抽</el-button>
+              <el-button type="primary" round id="lottery10">直接十连</el-button>
+            </el-container>
+          </el-footer>
         </el-container>
       </el-container>
     </el-container>
@@ -21,7 +38,15 @@ export default {
   name: "chouKa",
   data() {
     return {
-      msg: "111111111111"
+      diamond: "111111111111",
+      msg: "小熊",
+      imgWrap: [
+        {url: require("../assets/5.jpg")},
+        {url: require("../assets/4.jpg")},
+        {url: require("../assets/3.png")},
+        {url: require("../assets/2.png")},
+        {url: require("../assets/1.jpg")}
+      ]
     }
   },
   methods: {
@@ -31,7 +56,6 @@ export default {
   }
 }
 </script>
-
 <style scoped>
 .common-layout {
   height: 100%;
@@ -71,5 +95,38 @@ export default {
 
 .el-menu {
   background-color: #d3dce6;
+}
+
+#msg {
+  text-align: left;
+  width: 70%;
+}
+
+#diamond {
+  text-align: right;
+}
+
+#recharge {
+  text-align: left;
+  margin: 0;
+  border: 0;
+  padding: 0 67px 0 67px;
+}
+
+.picture {
+  width: 100%;
+  height: inherit;
+}
+
+#lottery01 {
+  width: 50%;
+  height: 100%;
+  text-align: center;
+}
+
+#lottery10 {
+  width: 50%;
+  height: 100%;
+  text-align: center;
 }
 </style>
