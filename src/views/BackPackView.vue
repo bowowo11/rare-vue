b7ceb0dbc3f9aa97b6a4bfdd6ea7e8d84015c753
huzihao <hzh553871310@163.com>
2022-06-30 20:13:23 +0800

    背包卡背

--- src/views/BackPackView.vue
+++ src/views/BackPackView.vue
@@ -3,17 +3,53 @@
     <h1>我的图鉴</h1>
   </div>
   <div class="mode1">
-      <p>二次元</p>
+      <p>UR</p>
+      <div class = "list">
+        <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+    <li v-for="i in ur" :key="i" class="infinite-list-item-ur">{{ i }}</li>
+  </ul>
+      </div>
   </div>
     <div class="mode1">
-      <p>二次元</p>
+      <p>SR</p>
+      <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+    <li v-for="j in sr" :key="j" class="infinite-list-item-sr">{{ j }}</li>
+  </ul>
       
   </div>
     <div class="mode1">
-      <p>原批</p>
+      <p>R</p>
+            <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+    <li v-for="j in r" :key="j" class="infinite-list-item-r">{{ j }}</li>
+  </ul>
   </div>
 
 </template>
+<script>
+export default {
+  name: "chouKa",
+  data() {
+    return {
+      //根据服务器的数据显示
+      ur: [
+        1,2,3
+      ],
+      sr:[
+        1,2,3,4,5,6,7
+      ],
+      r:[
+        1,2,3,4,5
+      ],
+      
+    }
+  },
+  methods: {
+    clickRecharge() {
+
+    }
+  }
+}
+</script>
 <style scoped>
 p{
     text-align: left;
@@ -21,7 +57,7 @@ p{
 }
 .mode1{
     /* height: 200px; */
-   margin-bottom: 200px;
+   /* margin-bottom: 200px; */
    border-radius: 20px;
    border: 10px;
 }
@@ -33,5 +69,52 @@ p{
     /* height: 200px; */
    margin-bottom: 200px;
 }
-
+.infinite-list {
+  display:grid;
+  grid-template-columns: repeat(5,20vw);
+  padding: 10px;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+.infinite-list .infinite-list-item-ur {
+   border:5px solid goldenrod;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  width: auto;
+  background: var(--el-color-primary-light-9);
+  margin: 10px;
+  color: var(--el-color-primary);
+}
+.infinite-list .infinite-list-item-sr {
+   border:5px solid plum;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  width: auto;
+  background: var(--el-color-primary-light-9);
+  margin: 10px;
+  color: var(--el-color-primary);
+}
+.infinite-list .infinite-list-item-r {
+   border:5px solid skyblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  width: auto;
+  background: var(--el-color-primary-light-9);
+  margin: 10px;
+  color: var(--el-color-primary);
+}
+.infinite-list .infinite-list-item + .list-item {
+  margin-top: 10px;
+}
+.list{
+  display: flex;
+}
 </style>
