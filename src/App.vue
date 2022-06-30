52bd3eecb0c3eaae077260b99ba2024160496441
huzihao <hzh553871310@163.com>
2022-06-30 16:10:39 +0800

    main.js配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/backPack">背包</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/BackPackView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>我的图鉴</h1>
+  </div>
+  <div class="mode1">
+      <p>二次元</p>
+  </div>
+    <div class="mode1">
+      <p>二次元</p>
+      
+  </div>
+    <div class="mode1">
+      <p>原批</p>
+  </div>
+
+</template>
+<style>
+p{
+    text-align: left;
+    background: aqua;
+}
+.mode1{
+    /* height: 200px; */
+   margin-bottom: 200px;
+   border-radius: 20px;
+   border: 10px;
+}
+.mode2{
+    /* height: 200px; */
+   margin-bottom: 200px;
+}
+.mode3{
+    /* height: 200px; */
+   margin-bottom: 200px;
+}
+
+</style>
