49f8e5dcbaa49d4b34104cd4a6bdf4cd91c8d020
huzihao <hzh553871310@163.com>
2022-07-01 12:43:39 +0800

    fuuuuuuuuuuuuck

--- src/views/BackPackView.vue
+++ src/views/BackPackView.vue
@@ -3,138 +3,140 @@
     <h1>我的图鉴</h1>
   </div>
   <div class="mode1">
-      <p>UR</p>
-      <div class = "list">
-        <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
-    <li v-for="i in ur" :key="i" class="infinite-list-item-ur">{{ i }}</li>
-  </ul>
-      </div>
+    <p>UR</p>
+    <div class="list">
+      <ul class="infinite-list">
+        <li v-for="item in imgWrap" :key="item.id">
+          <img :src="require('../picture/'+item.id+'.jpg')" class=".infinite-list-item-ur">
+        </li>
+      </ul>
+    </div>
   </div>
-    <div class="mode1">
-      <p>SR</p>
-      <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
-    <li v-for="j in sr" :key="j" class="infinite-list-item-sr">{{ j.name }}</li>
-  </ul>
-      
+  <div class="mode1">
+    <p>SR</p>
+    <ul class="infinite-list">
+      <li v-for="j in sr" :key="j">
+        {{ j.name }}
+      </li>
+    </ul>
   </div>
-    <div class="mode1">
-      <p>R</p>
-            <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
-    <li v-for="j in r" :key="j" class="infinite-list-item-r">{{ j }}</li>
-  </ul>
-  </div>
-
 </template>
 <script>
-
 export default {
   name: "chouKa",
-  // card:{
-  //   urList:[
-  //     {name:丁真,rank:ur},
-  //     {name:ayaya,rank:ur},
-  //     {name:niuniu,rank:ur},
-  //   ]
-
-  // },
   data() {
     return {
       //根据服务器的数据显示
-      ur: [
-        1,2,3,4,5
+      ur: [{ name: 1, rank: "ur", image: "3.png" }, 1, 2, 3, 4, 5],
+      sr: [
+        { name: 1, rank: "ur", image: "@/assets/5.jpg" },
+        { name: 22, rank: "ur" },
+        3,
+        4,
+        5,
+        6,
+        7,
       ],
-      sr:[
-        {name:1,rank:'ur'},{name:22,rank:'ur'},3,4,5,6,7
-      ],
-      r:[
-        1,2,3,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-      ],
-      
-    }
+      r: [1, 2, 3, 4, 5],
+      imgWrap: [],
+    };
   },
   methods: {
-    clickRecharge() {
+    clickRecharge() {},
+    clickUR(ID) {
+      console.log(ID);
+    },
+  },
 
-    }
-  }
-}
+  mounted: function () {
+    fetch("api/card")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.imgWrap = data})
+  },
+};
 </script>
 
 <style scoped>
-p{
-    text-align: left;
-    background: aqua;
+img{
+  overflow: auto;
+  border: 5px solid goldenrod;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  width: 350px;
+  background: var(--el-color-primary-light-9);
+  background-image: url("@/assets/5.jpg");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  margin: 10px;
+  color: var(--el-color-primary);
 }
-.mode1{
-    /* height: 200px; */
-   /* margin-bottom: 200px; */
-   border-radius: 20px;
-   border: 10px;
+p {
+  text-align: left;
+  background: aqua;
 }
-.mode2{
-    /* height: 200px; */
-   margin-bottom: 200px;
+.mode1 {
+  /* height: 200px; */
+  /* margin-bottom: 200px; */
+  border-radius: 20px;
+  border: 10px;
 }
-.mode3{
-    /* height: 200px; */
-   margin-bottom: 200px;
+.mode2 {
+  /* height: 200px; */
+  margin-bottom: 200px;
+}
+.mode3 {
+  /* height: 200px; */
+  margin-bottom: 200px;
 }
 .infinite-list {
-  
-  display:grid;
-  grid-template-columns: repeat(5,20vw);
+  display: grid;
+  grid-template-columns: repeat(5, 20vw);
   padding: 10px;
   height: 100%;
+  width: 100%;
   padding: 0;
   margin: 0;
   list-style: none;
 }
-.infinite-list .infinite-list-item-ur {
-   border:5px solid goldenrod;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 200px;
-  width: 350px;
-  background: var(--el-color-primary-light-9);
-  background-image: url("3.png");
-   background-size:100% 100%;
-  background-repeat:no-repeat;
-  margin: 10px;
-  color: var(--el-color-primary);
-}
+
 .infinite-list .infinite-list-item-sr {
-   border:5px solid plum;
+  overflow: auto;
+  border: 5px solid plum;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 200px;
   width: 350px;
   background: var(--el-color-primary-light-9);
-  background-image: url("3.png");
-   background-size:100% 100%;
-  background-repeat:no-repeat;
+  background-image: url("@/assets/3.png");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
   margin: 10px;
   color: var(--el-color-primary);
 }
 .infinite-list .infinite-list-item-r {
-   border:5px solid skyblue;
+  overflow: auto;
+  border: 5px solid skyblue;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 200px;
   width: 350px;
   background: var(--el-color-primary-light-9);
-  background-image: url("3.png");
-   background-size:100% 100%;
-  background-repeat:no-repeat;
+  background-image: url("@/assets/4.jpg");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
   margin: 10px;
   color: var(--el-color-primary);
 }
 .infinite-list .infinite-list-item + .list-item {
   margin-top: 10px;
 }
-.list{
+.list {
   display: flex;
 }
 </style>
